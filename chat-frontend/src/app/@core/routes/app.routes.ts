4b537c4e33df8ace76a1aa0bb4e6aa134b9f24d4
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-01-03 13:13:20 -0300

    refactor: simplify routes

--- chat-frontend/src/app/@core/routes/app.routes.ts
+++ chat-frontend/src/app/@core/routes/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
-import { CORE_ROUTES } from './router';
+import { MainLayoutComponent } from '../layout/main-layout.component';
+import { authGuard } from '../guards/auth.guard';
 import { AUTH_ROUTES } from '../../features/auth/router/auth.routes';
+import { HOME_ROUTES } from '../../features/home/router/home.routes';
 
 export const APP_ROUTES: Routes = [
-    ...AUTH_ROUTES,
-    ...CORE_ROUTES
+  ...AUTH_ROUTES,
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    canActivateChild: [authGuard],
+    children: [...HOME_ROUTES],
+  },
+
+  { path: '**', redirectTo: '' },
 ];
