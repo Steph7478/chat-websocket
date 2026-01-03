d26ad5d401905b96529689bfdca44a9d3176095c
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-01-03 12:24:35 -0300

    feat: login layout

---
+++ chat-frontend/src/app/features/auth/components/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'login-page',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginPage { }
