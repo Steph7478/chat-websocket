c4a5660be68719fdc22ae4b45d7f006a7629aa94
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-01-04 16:51:12 -0300

    feat: homepage layout

--- chat-frontend/src/app/features/home/pages/home.page.ts
+++ chat-frontend/src/app/features/home/pages/home.page.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CardComponent } from '../components/card/card.component';
 
 @Component({
   standalone: true,
   selector: 'home-page',
-  imports: [CommonModule, CardComponent],
   templateUrl: './home.page.html',
+  styleUrls: ['./home.page.css']
 })
 export class HomePage { }
