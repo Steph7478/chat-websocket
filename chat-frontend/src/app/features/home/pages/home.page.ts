b547d371344500ec59f44bd8172c2ca37e94f22c
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-01-05 16:51:18 -0300

    delete: unused import

--- chat-frontend/src/app/features/home/pages/home.page.ts
+++ chat-frontend/src/app/features/home/pages/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, signal, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ChatComponent } from '../components/chat/chat.component';
 
 @Component({
