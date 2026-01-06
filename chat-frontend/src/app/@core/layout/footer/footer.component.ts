db27a5aac2d5bdc13cc21fe4aa91eb77bdfed630
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-01-06 14:12:56 -0300

    style: footer

--- chat-frontend/src/app/@core/layout/footer/footer.component.ts
+++ chat-frontend/src/app/@core/layout/footer/footer.component.ts
@@ -1,11 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'footer',
     standalone: true,
     templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.css']
+    styleUrls: ['./footer.component.css'],
 })
+export class FooterComponent implements OnInit, OnDestroy {
+    time = '';
+    date = '';
+    private intervalId!: number;
 
-export class FooterComponent {
+    ngOnInit() {
+        this.updateClock();
+        this.intervalId = window.setInterval(() => {
+            this.updateClock();
+        }, 1000);
+    }
+
+    ngOnDestroy() {
+        clearInterval(this.intervalId);
+    }
+
+    private updateClock() {
+        const now = new Date();
+
+        const h = now.getHours().toString().padStart(2, '0');
+        const m = now.getMinutes().toString().padStart(2, '0');
+
+        this.time = `${h}:${m}`;
+
+        this.date = now.toLocaleDateString('pt-BR');
+    }
 }
